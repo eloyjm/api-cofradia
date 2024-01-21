b19ea7b31e79d12eae78e2dbfceb8e4be6a41ad4
eloyjm <eloyjm93@gmail.com>
2024-01-21 21:52:35 +0100

    feat: Pruebas nuevos modelos

--- src/testing/ropa.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-import tensorflow_datasets as tfds
-import numpy as np
-import matplotlib.pyplot as plt
-
-datos, metadatos = tfds.load('fashion_mnist', as_supervised=True, with_info=True)
